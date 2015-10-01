efd1558a8ead45fbdd28685e6ece3df941742686
Shinta Tri Wahyuning <shintatriwahyuning@gmail.com>
2015-10-01 17:29:49 +0700

    update simulation

---
+++ percobaan1/percobaan1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        If Me.BackColor = Color.Black Then
+            Me.BackColor = Color.Blue
+        Else
+            Me.BackColor = Color.Black
+        End If
+    End Sub
+End Class
